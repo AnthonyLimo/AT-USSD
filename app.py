9476aeea4e7a404c1e89e638d5e7d6d86e68e3f3
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-07-16 11:37:15 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET','POST'])
+
+def ussd():
+    session_id = request.values.get("sessionId",None)
+    servicecode =  request.values.get("serviceCode",None)
+    phone_number = request.values.get("phoneNumber",None)
+    text = request.values.get("text",None)
+
+    if text == "":
+        response = "CON What would you like to check?"
+        response = "1. My Account\n"
+        response = "2. My phone number\n"
+    elif text == "1":
+        response = "CON Choose a service that you require:\n"
+        response = "1. My account number\n"
+        response = "2. My account balance\n"
+    else:
+        response = "END Invalid choice"
+
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
